0b7ef7ed259872b28ece4349204a7af2f2a44d15
Vi <vinguyen7202@gmail.com>
2023-12-07 17:11:53 +0700

    Filtering function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,17 @@
 class Product < ApplicationRecord
   belongs_to :brand
 
+  #upload image
   mount_uploader :image, ImageUploader
 
+  #Data validation
   validates_presence_of :name, :price, :image
   validates_uniqueness_of :name
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ , message: "must have 2 decimal places"}, numericality: { greater_than: 0, less_than: 1000000 }
-  validates :discount, format: { with: /\A0\.\d\z/, message: "must have 0 followed by 2 decimal places" }
+  validates :discount, format: { with: /\A0\.\d{1,2}\z/, message: "must have 0 followed by up to 2 decimal places" }
+
+   def self.ransackable_attributes(auth_object = nil)
+    ["available", "brand_id", "created_at", "description", "discount", "image", "name", "price", "updated_at"]
+  end
+
 end
